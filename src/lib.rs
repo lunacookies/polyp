7988af5a7879094d0fec02fc9193da6d0bf17b7f
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-09-29 13:08:32 +1000

    Add initial types

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ServerMsg {
+    NewText(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ProcessletMsg {
+    NewOutput(Value),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Value {
+    Path(PathBuf),
+    List(Vec<Value>),
+}
